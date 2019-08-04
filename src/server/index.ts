c4e0ca7349280225e33606851502a0e95a6b4b53
erugis0345 <tl091264@gmail.com>
2019-08-04 10:14:24 +0900

    rest entry point

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,14 +7,25 @@ const app = Express()
 app.get('/', (req, res) => {
     const data = { message: 'pong' }
     res.send(data)
-
 })
 
-// boot Express
+app.get('/rest', (req, res) => {
+    res.send('GET request')
+})
+app.post('/rest', (req, res) => {
+    res.send('POST request')
+})
+app.put('/rest', (req, res) => {
+    res.send('PUT request')
+})
+app.delete('/rest', (req, res) => {
+    res.send('DELETE request')
+})
 
+
+// boot Express
 const port = 8888
 const host = 'localhost'
-
 app.listen(port, host, () => {
     console.log(`Running on http://${host}:${port}`)
 })
