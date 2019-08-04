906f2d92fca4ccaf22dd1244300820f6c6599d82
erugis0345 <tl091264@gmail.com>
2019-08-04 10:07:56 +0900

    serve on express

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello, typeScript!!!')
+import Express from 'express'
+
+const app = Express()
+
+// Routing
+
+app.get('/', (req, res) => {
+    const data = { message: 'pong' }
+    res.send(data)
+
+})
+
+// boot Express
+
+const port = 8888
+const host = 'localhost'
+
+app.listen(port, host, () => {
+    console.log(`Running on http://${host}:${port}`)
+})
